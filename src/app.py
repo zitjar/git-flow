c0895d5584791d742dbb79f99cc6422c4c8a409c
Alan Celaya <alangiovannyc@gmail.com>
2024-03-24 16:18:54 -0600

    Fix: Arreglando errores en las rutas y saludos. AC

--- src/app.py
+++ src/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return "Hola"
+
+@app.route('/hello')
+def graetings():
+    return 'Hello world guapisimos!'
 
 app.route('/sum/<int:a>/<int:b>')
 def sum(a:int,b:int):
